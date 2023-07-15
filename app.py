44beac2ec26fc37215cef22901330a7d6b90796a
Evan Cromwell <123791177+eacromwell@users.noreply.github.com>
2023-07-15 00:50:17 -0600

    Fixed redundant variables + API Tobs Path

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def welcome():
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation - Precipitation data for the last year<br/>"
         f"/api/v1.0/stations - List of weather stations<br/>"
-        f"/api/v1.0/tobs - Temperature observations for the last year<br/>"
+        f"/api/v1.0/tobs - Temperature observations of the most-active station for the previous year of data.<br/>"
         f"/api/v1.0/start_date - Min, Avg, and Max temperature for a given start date (format: YYYY-MM-DD)<br/>"
         f"/api/v1.0/start_date/end_date - Min, Avg, and Max temperature for a given start date and end date (format: YYYY-MM-DD)"
     )
@@ -68,6 +68,7 @@ def precipitation():
     one_year_ago = dt.date.fromisoformat(most_recent_date) - dt.timedelta(days=365)
     results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= one_year_ago).all()
     precipitation_data = {date: prcp for date, prcp in results}
+    session.close()  # Added session close
     return jsonify(precipitation_data)
 
 
@@ -81,6 +82,7 @@ def stations():
     results = session.query(Station.station).all()
     # Convert the results to a list
     station_list = list(np.ravel(results))
+    session.close()  # Added session close
     return jsonify(station_list)
 
 #################################################
@@ -93,10 +95,19 @@ def tobs():
     most_recent_date = session.query(func.max(Measurement.date)).scalar()
     # Calculate the date one year from the last date in data set
     one_year_ago = dt.date.fromisoformat(most_recent_date) - dt.timedelta(days=365)
-    # Query temperature observations for the last year
-    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= one_year_ago).all()
-    # Create a dictionary with date as the key and temperature as the value
-    temperature_data = {date: tobs for date, tobs in results}
+    # Query the most active station based on the highest count of temperature observations
+    most_active_station = session.query(Measurement.station)\
+        .group_by(Measurement.station)\
+        .order_by(func.count(Measurement.tobs).desc())\
+        .first()
+    # Query temperature observations for the most active station in the last year
+    results = session.query(Measurement.date, Measurement.tobs)\
+        .filter(Measurement.date >= one_year_ago)\
+        .filter(Measurement.station == most_active_station[0])\
+        .all()
+    # Create a list of dictionaries with date and temperature observations
+    temperature_data = [{'date': date, 'tobs': tobs} for date, tobs in results]
+    session.close()  # Close the session
     return jsonify(temperature_data)
 
     
@@ -115,6 +126,7 @@ def temperature_by_start_date(start):
         "Avg Temperature": results[0][1],
         "Max Temperature": results[0][2]
     }
+    session.close()  # Added session close
     return jsonify(temperature_data)
 
     
@@ -133,6 +145,7 @@ def temperature_by_start_end_date(start, end):
         "Avg Temperature": results[0][1],
         "Max Temperature": results[0][2]
     }
+    session.close()  # Added session close
     return jsonify(temperature_data)
 if __name__ == '__main__':
     app.run(debug=True)
